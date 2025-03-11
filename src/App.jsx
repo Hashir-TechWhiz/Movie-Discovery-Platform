ca8bef56d1ee8f204620f9a0f8b52d021f346d37
Muhammad Hashir <hashirmohamed04@gmail.com>
2025-03-11 17:52:18 +0530

    Hero section on going

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import './App.css'
-
 function App() {
+    return (
+        <main>
+            <div className="pattern" />
 
-  return (
-   <div>Hello World!</div>
-  )
+            <div className="wrapper">
+                <div className="flex justify-center">
+                    <img src="./logo.png" alt="Logo" width={60} height={60} />
+                </div>
+
+                <header>
+                    <img src="./hero.png" alt="Hero" />
+                    <h1>
+                        Find <span className="text-gradient">Movies</span> You'll Enjoy
+                        Without the Hassle
+                    </h1>
+                </header>
+            </div>
+        </main>
+    );
 }
 
-export default App
+export default App;
